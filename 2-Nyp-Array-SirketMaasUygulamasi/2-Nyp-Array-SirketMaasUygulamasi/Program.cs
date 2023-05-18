fe515d44321d5941253b50650007c3df49f6d1a3
Teoman BAŞ <teomanbas@protonmail.com>
2023-05-18 00:12:18 -0400

    Yeni Ornekler Eklendi.

---
+++ 2-Nyp-Array-SirketMaasUygulamasi/2-Nyp-Array-SirketMaasUygulamasi/Program.cs
@@ -0,0 +1,58 @@
+﻿class calisan
+{
+    //getter
+    string isim { get; }
+    public double maas { get; set; }
+    // const
+    public calisan(string isim, int maas)
+    {
+        this.isim = isim;
+        this.maas = maas;
+    }
+    public void zam(double oran)
+    {
+        maas = maas * ( 1 + oran);
+    }
+
+    // ToString metodu normalde tüm objelerin metodudur her objenin ToString metodu vardır.bunun için daha önce olan 
+    // ToString metodunun üzerinde override etmek durumundayız
+    override public string ToString()
+    {
+        return "calisan ismi : " + isim + " | maas : " + maas;
+    }
+}
+internal class Program
+{
+    static void zam(calisan[]s, double oran)
+    {
+        for (int i = 0;i< s.Length; i++)
+        {
+            s[i].zam(oran);
+        }
+    }
+    static void EkranaBas(calisan[] s)
+    {
+        foreach(calisan x in s)
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        calisan[] sirket = new calisan[5];
+        sirket[0] = new calisan("ali", 1000);
+        sirket[1] = new calisan("veli", 230);
+        sirket[2] = new calisan("fatma", 3000);
+        sirket[3] = new calisan("ahmet", 2000);
+        sirket[4] = new calisan("deniz", 1500);
+        EkranaBas(sirket);
+        zam(sirket, 0.2);
+        Console.WriteLine("---------------------------");
+        EkranaBas(sirket);
+        zam(sirket, 0.2);
+        Console.WriteLine("---------------------------");
+        EkranaBas(sirket);
+
+    }
+}
---
+++ 3-Nyp-linq-list-GenericTip_Uygulamasi/3-Nyp-linq-list-GenericTip_Uygulamasi/Program.cs
@@ -0,0 +1,55 @@
+﻿class ogrenci
+{
+    public string Isim;
+    public double NotOrtalamasi;
+    public string Bolum;
+    public ogrenci(string ısim, double notOrtalamasi, string bolum)
+    {
+        this.Isim = ısim;
+        this.NotOrtalamasi = notOrtalamasi;
+        this.Bolum = bolum;
+    }
+
+    public override string ToString()
+    {
+        return "isim : " + Isim + " bolum : " + Bolum + " Not Ortalaması : " + NotOrtalamasi;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ogrenci ali = new ogrenci("ayşe", 90, "bilgisayar");
+        List <ogrenci> sinif=new List<ogrenci>();
+        sinif.Add(ali);
+        Console.WriteLine("doğrudan obje : "+ali);
+        Console.WriteLine("listeden ilk eleman"+ sinif.ElementAt(0));
+        Console.WriteLine("listeden ilk eleman" + sinif[0]);
+        Console.WriteLine("listenin kapasitesi" + sinif.Capacity);
+        sinif.Add(new ogrenci("ahmet",80, "felsefe"));
+        sinif.Add(new ogrenci("deniz",78, "bilgisayar"));
+        sinif.Add(new ogrenci("cem",40, "bilgisayar"));
+        sinif.Add(new ogrenci("fatma",70, "coğrafya"));
+        sinif.Add(new ogrenci("berk",80, "matematik"));
+        Console.WriteLine("listeden ilk eleman" + sinif.ElementAt(0));
+        Console.WriteLine("listeden ilk eleman" + sinif[0]);
+        Console.WriteLine("listenin kapasitesi" + sinif.Capacity);
+
+        var sonuc =
+            from x in sinif
+            where x.NotOrtalamasi > 50
+            orderby x.Isim descending
+            select x.Isim;
+        foreach ( var x in sonuc)
+        {
+            Console.WriteLine(x);
+        }
+        
+
+
+
+
+
+
+    }
+}
